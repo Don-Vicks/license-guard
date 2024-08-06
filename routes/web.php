25247ca15c6547984ff2aa8aeaba7443da8a011d
venomous-maker <morganokumu8@gmail.com>
2024-08-07 02:43:49 +0300

    If logged-in user is admin redirect to /admin

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Checkout;
 use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 Route::get('/', function () {
-    return redirect('/user');
+    $user = Auth::user();
+
+    if ($user) {
+        if (Gate::allows('access-admin-panel')) {
+            return redirect('/admin');
+        } else {
+            return redirect('/user');
+        }
+    } else {
+        return redirect('/login'); // or wherever you want unauthenticated users to go
+    }
 });
+
 
 Route::get('checkout/{id}', [Checkout::class, 'checkout'])->name('checkout')->middleware('auth');
 Route::get('checkout/verify/confirm', [Checkout::class, 'verify'])->name('checkout.verify')->withoutMiddleware('web');
