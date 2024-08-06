91120cf16ae4a4b7989fd9e41a6c098b67439c9a
venomous-maker <morganokumu8@gmail.com>
2024-08-06 15:04:11 +0300

    Removed active url checking

--- app/Http/Controllers/API/LicenseController.php
+++ app/Http/Controllers/API/LicenseController.php
@@ -12,10 +12,10 @@ class LicenseController extends Controller
 {
     public function validate(Request $request){
         $validator = Validator::make($request->all(), [
-            'domain' => 'active_url|required',
+            'domain' => 'required', //'domain' => 'active_url|required',
             'license_key' => 'required|exists:licenses,key',
         ]);
-    
+
         if ($validator->fails()) {
             // You can customize the response when validation fails
             return response()->json(['error' => $validator->errors()], 400);
@@ -28,12 +28,12 @@ class LicenseController extends Controller
         ->where('link', $link)
         ->where('active', 1)
         ->first();
-    
- 
+
+
         if($check){
             return response()->json(['message' => 'Horray, Your activation has been confirmed'], 200);
         } else{
             return response()->json(['message' => 'Whoops, something isn\'t right, Kindly recheck the submitted details'], 401);
-        }       
+        }
     }
 }
