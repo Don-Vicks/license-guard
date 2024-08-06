0ce014f103cbb53c587b37c0257540cc6c9f0d46
venomous-maker <morganokumu8@gmail.com>
2024-08-06 14:49:41 +0300

    Added necessary fields to table

--- database/migrations/2024_07_24_114100_create_add_trx_ref.php
+++ database/migrations/2024_07_24_114100_create_add_trx_ref.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('payments', function (Blueprint $table) {
-            // $table->string('trx_ref')->nullable()->after('user_id');
-            // $table->string('licensetype_id')->nullable()->after('trx_ref');
+            $table->string('trx_ref')->nullable()->after('user_id');
+            $table->string('licensetype_id')->nullable()->after('trx_ref');
             $table->string('license_id')->nullable()->after('trx_ref');
         });
     }
