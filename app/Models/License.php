e59f8a3d16d8767885e68099b5320f97e03107a4
venomous-maker <morganokumu8@gmail.com>
2024-08-07 17:31:54 +0300

    Fixed relationships

--- app/Models/License.php
+++ app/Models/License.php
@@ -11,18 +11,19 @@ class License extends Model
         'user_id',
         'link',
         'expiry_date',
+        'type_id',
         'key'
     ];
     use HasFactory;
 
     public function user()
     {
-        return $this->belongsTo(User::class); 
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function type()
     {
-        return $this->belongsTo(LicenseType::class); 
+        return $this->belongsTo(LicenseType::class, 'type_id');
     }
-    
+
 }
