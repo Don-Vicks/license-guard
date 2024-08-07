0f3e6e714fc237b0e753ecf911c378c021443a9d
venomous-maker <morganokumu8@gmail.com>
2024-08-07 17:31:26 +0300

    Added new payments as a dashboard widget

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use App\Filament\Widgets\NewPayments;
 use App\Filament\Widgets\NewUsers;
 use App\Filament\Widgets\StatsOverview;
 use Filament\Pages\Page;
@@ -17,6 +18,7 @@ class Dashboard extends Page
         return [
             StatsOverview::class,
             NewUsers::class,
+            NewPayments::class,
         ];
     }
 }
