4190c14f593cbc69c6435c158a48914790e909df
atriexz4pato <kungapatrick81@gmail.com>
2024-08-16 20:49:57 +0300

    Update Daraja Api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\MpesaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\LicenseController;
 use App\Http\Controllers\CronController;
 
 Route::get('validate/license', [LicenseController::class, 'validate'])->name('validate.license');
 Route::get('run/cronjob', [CronController::class, 'job']);
+Route::post('/stkPush', [MpesaController::class, 'stkPush'])->name('stkPush');
+
