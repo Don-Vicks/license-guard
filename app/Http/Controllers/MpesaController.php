685391341be1bbc225db96d25412ab8bc3c67de9
atriexz4pato <kungapatrick81@gmail.com>
2024-08-16 20:50:15 +0300

    Introduce mpesa controller

---
+++ app/Http/Controllers/MpesaController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MpesaTransaction;
+use App\Services\MpesaService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class MpesaController extends Controller
+{
+    /**
+     * @param StkPushRequest $request
+     * @return string[]
+     */
+    public function stkPush(StkPushRequest $request)
+    {
+        $mpesaService = new MpesaService();
+        $data = [
+            'amount' => $request->amount,
+            'phoneNumber' => $request->phoneNumber,
+            'transactionType' => 'CustomerPayBillOnline',
+            'accountReference' => 'accountReference',
+
+        ];
+        $response = $mpesaService->sendSTKPush($data);
+        $transaction = MpesaTransaction::query()
+            ->create([
+                'MerchantRequestID' => $response['response']['MerchantRequestID'],
+                'CheckoutRequestID' => $response['response']['CheckoutRequestID'],
+                'phone_number' => $request->phoneNumber,
+                'transaction_amount' => $request->amount,
+            ]);
+
+        if ($transaction) {
+            return [
+                'success' => true,
+                'transactionDateTime' => $transaction->created_at,
+                'transaction' => $transaction
+            ];
+        }
+        return [
+            'success' => 'error',
+            'message' => 'Failed to initiate Transaction'
+        ];
+    }
+
+    public function syncPayment()
+    {
+        return [
+            'success' => true,
+        ];
+    }
+
+    public function confirmation(Request $request){
+        $transaction = MpesaTransaction::query()
+            ->where('phone_umber',$request->phoneNumber)
+            ->where('transaction_amount',$request->amount)
+            ->whereBetween('transaction_date',[Carbon::parse($request->dateTime),Carbon::parse($request->dateTime)->endOfMonth()])
+            ->first();
+        if($transaction){
+            //return the transaction
+            return [
+                'success' => true,
+                'message' => _('Transaction Confirmed'),
+                'amount' => $transaction->transaction_amount,
+                'transaction' => $transaction
+            ];
+        }
+        return [
+            'success' => false,
+            'message' => _('Transaction Not Found'),
+        ];
+    }
+}
